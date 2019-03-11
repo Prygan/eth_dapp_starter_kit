c44298d0945352c86f54b06eeb55827523bbe9e4
Dimitri Saingre <dimitrisaingre@orange.fr>
2019-03-11 16:18:37 +0100

    add some doc on arrow functions

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@
     // voir : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Strict_mode
     'use strict';
 
+    // La flèche est une syntaxe pour une fonction fléchée
+    // voir : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions/Fonctions_fl%C3%A9ch%C3%A9es
+    // c'est une fonction qui prend en paramètre un objet (ici web3) et effectue un traitement dessus
     initialiseWeb3().then((web3) => {
         // C'est ici qu'on pourrait mettre notre code.
 
